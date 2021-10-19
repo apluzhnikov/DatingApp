007047732194ff4dfb254486f8160b5f81a7da7c
Pluzhnikov Anton <pluzhnikov.anton@hotmail.com>
2021-10-19 16:40:17 +0300

    End of section 4

---
+++ API/DTOs/RegiterDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class RegiterDto
+    {
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,29 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //private readonly IConfiguration _config = builder.Services.;
 // Add services to the container.
+
+/*builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+});*/
 
+builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddCors();
+
+builder.Services.AddIdentityServices(builder.Configuration);
 
 builder.Services.AddSwaggerGen(c =>
 {
@@ -38,6 +49,7 @@ app.UseCors(policy => policy.AllowAnyHeader()
                             .AllowAnyMethod()
                             .WithOrigins("https://localhost:4200"));
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
